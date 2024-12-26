9ef2c1fbf6ca35ff8b620a52ffbaee9ad3a74cf4
ebi002 <126892942+ebi002@users.noreply.github.com>
2024-12-26 11:05:31 +0530

    updated axios

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,10 +3,19 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8000",
 });
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("jwtToken"); // Retrieve token from sessionStorage
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`; // Attach token to headers
+    }
+    return config;
+  },
+  (error) => Promise.reject(error) // Handle request errors
+);
 
 axiosInstance.interceptors.response.use(
   (response) => {
-    
     const token = response.data.token;
     if (token) {
       sessionStorage.setItem("jwtToken", token); // Save the token in sessionStorage
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "../axios/axios";
 
 function Home() {
   const navigate = useNavigate();
+
   const handleLogOut = () => {
     sessionStorage.removeItem("jwtToken");
     navigate("/login");
   };
+  const handleData=async(e)=>{
+    const response=await axiosInstance.get("http://localhost:8000/user/allusers")
+    console.log(response.data.users)
+  }
   return (
     <>
-    <div className="flex justify-between ">
-      <h1 className="font-roboto text-4xl">Home Page</h1>
-      <button onClick={handleLogOut} className="bg-red text-white text-col px-6 py-2 rounded-xl mr-2 mt-2 font-semibold">Log Out</button>
-    </div>
+      <div className="flex justify-between ">
+        <h1 className="font-roboto text-4xl">Home Page</h1>
+        <button
+          onClick={handleLogOut}
+          className="bg-red text-white text-col px-6 py-2 rounded-xl mr-2 mt-2 font-semibold"
+        >
+          Log Out
+        </button>
+      </div>
+      <div className="flex">
+      <button className="bg-lightGreen text-white text-col px-6 py-2 rounded-xl mx-auto font-semibold" onClick={handleData}>
+        Get User Details
+      </button>
+      </div>
     </>
   );
 }
